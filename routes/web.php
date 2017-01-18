ebfa3b8fa6e8f41c6c2eec90c7f97e31aa9f10b7
Mamun Morshed <imroony@gmail.com>
2017-01-19 01:13:28 +0600

    Added authentication to the app. Disabled user registration feature.

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Auth::routes();
+// Route::get('/login', 'Auth\LoginController@showLoginForm' );
+// Route::post('/login', 'Auth\LoginController@login');
+// Route::post('/logout', 'Auth\LoginController@logout');
+
+Route::get('login', ['as' => 'login', 'uses' => 'Auth\LoginController@showLoginForm']);
+Route::post('login', ['as' => 'login.post', 'uses' => 'Auth\LoginController@login']);
+Route::post('logout', ['as' => 'logout', 'uses' => 'Auth\LoginController@logout']);
+
+Route::get('/home', 'HomeController@index');
